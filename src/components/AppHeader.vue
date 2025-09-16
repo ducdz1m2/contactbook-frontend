5dce8113a20ea9a2d22bf266875e1e8964c58253
ducdz1m2 <93931868+ducdz1m2@users.noreply.github.com>
2025-09-16 13:06:50 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!-- Dùng router-link thay vì <a> -->
+        <router-link :to="{ name: 'contactbook' }" class="navbar-brand">
+            Ứng dụng quản lý danh bạ
+        </router-link>
+
+        <div class="navbar-nav">
+            <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                Danh bạ
+                <i class="fas fa-address-book"></i>
+            </router-link>
+        </div>
+    </nav>
+</template>
---
+++ src/views/ContactBook.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="page container">
+        <div class="row mb-3">
+            <div class="col-12">
+                <InputSearch v-model="searchText" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h4>
+                    Danh bạ <i class="fas fa-address-book"></i>
+                </h4>
+                <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts"
+                    v-model:activeIndex="activeIndex" />
+                <p v-else>Không có liên hệ nào.</p>
+
+                <div class="mt-3 d-flex justify-content-between">
+                    <button class="btn btn-sm btn-success" @click="goToAddContact">
+                        <i class="fas fa-plus"></i> Thêm mới
+                    </button>
+                    <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+                        <i class="fas fa-trash"></i> Xóa tất cả
+                    </button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div v-if="activeContact">
+                    <h4>
+                        Chi tiết liên hệ <i class="fas fa-address-card"></i>
+                    </h4>
+                    <ContactCard :contact="activeContact" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import ContactCard from '@/components/ContactCard.vue';
+import ContactList from '@/components/ContactList.vue';
+import InputSearch from '@/components/InputSearch.vue';
+import contactService from '@/services/contact.service';
+import { computed } from 'vue';
+
+export default {
+    components: {
+        ContactCard,
+        InputSearch,
+        ContactList,
+    },
+    data() {
+        return {
+            contacts: [],
+            activeIndex: -1,
+            searchText: "",
+        };
+    },
+    computed: {
+        filteredContacts() {
+            if (!this.searchText) return this.contacts;
+
+            const text = this.searchText.toLowerCase();
+            return this.contacts.filter((contact) => {
+                return (
+                    contact.name?.toLowerCase().includes(text) ||
+                    contact.email?.toLowerCase().includes(text) ||
+                    contact.phone?.toLowerCase().includes(text)
+                );
+            });
+        },
+
+        filteredContactsCount() {
+            return this.filteredContacts.length;
+        },
+        activeContact() {
+            if (this.activeIndex < 0) return null;
+            return this.filteredContacts[this.activeIndex];
+        }
+    },
+    methods: {
+        async retrieveContacts() {
+            try {
+                this.contacts = await contactService.getAll();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        refreshList() {
+            this.retrieveContacts();
+            this.activeIndex = -1;
+        },
+        async removeAllContacts() {
+            if (confirm("Bạn muốn xóa tất cả liên hệ?")) {
+                try {
+                    await contactService.deleteAll();
+                    this.refreshList();
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+        goToAddContact() {
+            this.$router.push({ name: "contact.add" });
+        },
+
+    },
+    mounted() {
+        this.refreshList();
+    }
+};
+</script>
+
+<style>
+.page {
+    text-align: left;
+    max-width: 750px;
+}
+</style>
